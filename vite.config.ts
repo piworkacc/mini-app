7b86d91a2f0e4ab7f1a2a52b74a9da78b3054b00
Иван Пузырёв <i.puzyrev@sbereducation.ru>
2025-03-17 12:43:21 +0300

    changed to math keyboard

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  base: '/mini-app/'
+  build: {
+    outDir: './docs',
+  },
+  base: './',
+  server: {
+    port: 5174,
+  },
 });
