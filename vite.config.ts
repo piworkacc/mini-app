202df3624dfe4e53f88fb5979127b50dda9a0368
Иван Пузырёв <i.puzyrev@sbereducation.ru>
2025-03-17 12:53:43 +0300

    Revert "changed to math keyboard"
    This reverts commit 7b86d91a2f0e4ab7f1a2a52b74a9da78b3054b00.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl';
-import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  build: {
-    outDir: './docs',
-  },
-  base: './',
-  server: {
-    port: 5174,
-  },
+  base: '/mini-app/'
 });
