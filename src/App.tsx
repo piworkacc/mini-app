6002019b523a9d95875fbfcc4272be7372a3fd71
Иван Пузырёв <i.puzyrev@sbereducation.ru>
2025-03-17 15:24:34 +0300

    changed to math keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ export default function App() {
                     setValue(e);
                 }}
             />
-            <MainButton text="Готово" onClick={handlerSendData} progress={true} hasShineEffect={true} />
+            <MainButton text="Готово" onClick={handlerSendData}  />
         </>
     );
 }
