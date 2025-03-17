f09d33b04462c594ab8a74b3df1f17a2eb071391
Иван Пузырёв <i.puzyrev@sbereducation.ru>
2025-03-17 20:31:38 +0300

    changed to math keyboard

--- src/hooks/useMainButton/index.tsx
+++ src/hooks/useMainButton/index.tsx
@@ -21,7 +21,7 @@ export function useMainButton(value: string) {
       mainButton.setParams({
         isVisible: true,
         text: "Отправить",
-        isEnabled: true,
+        isEnabled: value.length > 0,
       });
     }
 
