4d1b8201296be0c74c8fe0de46749618f24e9451
Maxim Rozhkov <maks.rozhkov.2004@gmail.com>
2025-04-03 15:39:25 +0500

    try fix for telegram mini app

--- src/components/MathField/index.tsx
+++ src/components/MathField/index.tsx
@@ -78,6 +78,7 @@ const MathField: FC<Props> = ({
   mf.style.setProperty('width', '100%');
 
   useEffect(() => {
+    window.mathVirtualKeyboard.targetOrigin="*";
     window.mathVirtualKeyboard.show();
 
     if (mf && containerRef.current) {
